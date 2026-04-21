9b146f3a8d45c9f3270747b32099e334fcf866f0
vDhinithya <dhinithya12@gmail.com>
2026-04-21 14:29:23 +0530

    implemented application service

--- application-service/src/main/java/com/jobportal/application/service/ApplicationService.java
+++ application-service/src/main/java/com/jobportal/application/service/ApplicationService.java
@@ -4,28 +4,44 @@ import com.jobportal.application.client.JobClient;
 import com.jobportal.application.entity.Application;
 import com.jobportal.application.repository.ApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApplicationService {
+
     @Autowired
     private ApplicationRepository repository;
 
     @Autowired
     private JobClient jobClient;
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     public Application applyToJob(String jobId, String userId) {
-        // CALLING JOB-SERVICE VIA FEIGN
+        // 1. Verify via OpenFeign
         try {
             jobClient.getJobById(jobId);
         } catch (Exception e) {
             throw new RuntimeException("Job not found! You cannot apply.");
         }
 
+        // 2. Save to MongoDB
         Application app = new Application();
         app.setJobId(jobId);
         app.setApplicantId(userId);
         app.setStatus("APPLIED");
-        return repository.save(app);
+        Application savedApp = repository.save(app);
+
+        // 3. FIRE THE KAFKA EVENT!
+        // We are constructing a simple JSON string to send to the broker
+        String message = String.format("{\"applicationId\":\"%s\", \"jobId\":\"%s\", \"userId\":\"%s\"}",
+                savedApp.getId(), jobId, userId);
+
+        kafkaTemplate.send("notificationTopic", message);
+        System.out.println(" KAFKA EVENT FIRED: " + message);
+
+        return savedApp;
     }
 }
